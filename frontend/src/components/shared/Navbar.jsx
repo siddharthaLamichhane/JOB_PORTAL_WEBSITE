8b33330bcce247312cc88f0a36ea1bc12b4b58fc
siddhartha lamichhane <siddharthalamichhane56@gmail.com>
2025-12-26 23:07:42 +0545

    doing some  margining stuff

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,27 +1,63 @@
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button.jsx";
+import { User, LogOut } from "lucide-react";
+
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover.jsx";
+
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar.jsx";
+
 export const Navbar = () => {
   return (
-    <div className="bg-white">
-      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+    <div className="bg-white border-b">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16 px-4">
         <h1 className="text-2xl font-bold">
           Job<span className="text-[#f83002]">Portal</span>
         </h1>
 
-        <div>
-          <ul className="flex items-center font-medium gap-5">
-            <li>Home</li>
-            <li>Jobs</li>
-            <li>Browse</li>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button variant="outline">Open PopOver</Button>
-                <PopoverContent>hello</PopoverContent>
-              </PopoverTrigger>
-            </Popover>
-          </ul>
-        </div>
+        <ul className="flex items-center font-medium gap-6">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">Jobs</li>
+          <li className="cursor-pointer">Browse</li>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>SL</AvatarFallback>
+              </Avatar>
+            </PopoverTrigger>
+
+            <PopoverContent className="w-80">
+              {/* Profile info */}
+              <div className="flex items-center gap-4 mb-4">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>SL</AvatarFallback>
+                </Avatar>
+
+                <div>
+                  <h4 className="font-medium">Siddhartha Lamichhane</h4>
+                  <p className="text-sm text-muted-foreground">
+                    Computer Engineer · Full Stack Developer
+                  </p>
+                </div>
+              </div>
+
+              {/* Actions */}
+              <div className="flex flex-col text-gray-600">
+                <div className="flex items-center gap-2">
+                  <User size={18} />
+                  <Button variant="link">View Profile</Button>
+                </div>
+
+                <div className="flex items-center gap-2">
+                  <LogOut size={18} />
+                  <Button variant="link">Logout</Button>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </ul>
       </div>
     </div>
   );
